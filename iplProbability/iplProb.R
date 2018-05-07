60bdf1a38e29c88c9876987db3521fb6be712513
Arunram Atmacharan <arunram.atma@gmail.com>
2018-05-07 22:34:33 +0530

    Refactoring code; Fixing a bug in loop ranging

--- iplProbability/iplProb.R
+++ iplProbability/iplProb.R
@@ -1,30 +1,32 @@
 Sys.time()
 library(data.table)
+
 nrr <- c(0.471, 0.421, 0.145, 0.130, 0.070, -0.376, -0.411, -0.726)
 teams <- c("SRH", "CSK", "KKR", "KXIP", "MI", "RCB", "DD", "RR")
-tally <- c(14, 14, 10, 10, 6, 6, 6, 6)
-tally <- tally + nrr 
+basePts <- c(14, 14, 10, 10, 6, 6, 6, 6)
 
+tally <- basePts + nrr 
 names(tally) <- teams 
 
-homes <- c("MI", "KXIP", "SRH", "RR", "KKR", "DD", "RR", "KXIP", "DD", "CSK", 
+hosts <- c("MI", "KXIP", "SRH", "RR", "KKR", "DD", "RR", "KXIP", "DD", "CSK", 
     "MI", "KXIP", "KKR", "MI", "RCB", "DD", "RR", "SRH", "DD", "CSK")
-aways <- c("KKR", "RR", "RCB", "KXIP", "MI", "SRH", "CSK", "KKR", "RCB", "SRH",
+visitors <- c("KKR", "RR", "RCB", "KXIP", "MI", "SRH", "CSK", "KKR", "RCB", "SRH",
     "RR", "RCB", "RR", "KXIP", "SRH", "CSK", "RCB", "KKR", "MI", "KXIP")
-    
-totalMatches <- length(homes)
-    
-results <- c("MI", "KXIP")
-numResults <- length(results)
 
-remMatches <- c(numResults + 1 : totalMatches)
-curHomes <- homes[remMatches]
-curAways <- aways[remMatches]
+# Update the results here, after each match is completed    
+results <- c("MI", "KXIP") #, "SRH", "RR", "KKR", "DD", "RR")
 
-getWins <- function(x){return(length(which(results==x)))}
+remMatches <- seq(hosts)[-seq(results)]
+curHosts <- hosts[remMatches]
+curVisitors <- visitors[remMatches]
 
-incrScores <- 2 * sapply(teams, getWins)      # 2 points for a win  
-newScores <- tally + incrScores
+# This is a closure: return value is a function
+getWins <- function(y) { return(function(x){return(length(which(y==x)))}) }
+# getPoints is for all teams whereas getWins is for a particular team
+getPoints <- function(x) { return(2 * sapply(teams, getWins(x))) }
+
+resultPoints <- getPoints(results)
+curTally <- tally + resultPoints
 
 numRemMatches <- length(remMatches)
 numRoutes <- 2 ^ numRemMatches
@@ -35,13 +37,12 @@ for(i in teams){
     assign(i, 0)    
 }
 
-for (i in c(0:numRoutes-1)){
+for (i in (seq(numRoutes)-1)){
     homeWins <- rev(as.integer(intToBits(i))[1:numRemMatches])
-    winTeams <- ifelse(homeWins, curHomes, curAways)
-    scoreFn <- function(x){return(length(which(winTeams==x)))}
-    fwdScores <- 2 * sapply(teams, scoreFn)
-    roundScores <- newScores + fwdScores
-    top4 <- names(sort(roundScores, decreasing=TRUE)[1:4])
+    winTeams <- ifelse(homeWins, curHosts, curVisitors)
+    futurePoints <- getPoints(winTeams)
+    finalPoints <- curTally + futurePoints
+    top4 <- names(sort(finalPoints, decreasing=TRUE)[1:4])
     sapply(top4, addFn)
 }
 
